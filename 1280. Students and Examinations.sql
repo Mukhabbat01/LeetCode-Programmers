9f60835ef1c334fa1ce11403b047343d8509a585
Abduzhalilova Mukhabbat <148844893+Mukhabbat01@users.noreply.github.com>
2024-09-23 18:30:42 +0900

    Update 1280. Students and Examinations.sql

--- 1280. Students and Examinations.sql
+++ 1280. Students and Examinations.sql
@@ -4,3 +4,8 @@ cross join subjects j
 left join examinations e on s.student_id = e.student_id and j.subject_name = e.subject_name
 group by s.student_id, s.student_name, j.subject_name
 order by s.student_id, j.subject_name;
+
+
+
+#leetcode 1280번 sql 문제
+
