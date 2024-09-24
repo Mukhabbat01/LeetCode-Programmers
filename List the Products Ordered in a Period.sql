7cffd7f4d8d234166c7da2ccc036dd1bdf531e37
Abduzhalilova Mukhabbat <148844893+Mukhabbat01@users.noreply.github.com>
2024-09-24 23:46:21 +0900

    Update List the Products Ordered in a Period.sql

--- List the Products Ordered in a Period.sql
+++ List the Products Ordered in a Period.sql
@@ -4,3 +4,5 @@ left join products p on p.product_id = o.product_id
 where date_format(o.order_date, '%Y-%m') = '2020-02' 
 group by o.product_id
 having unit >= 100;
+
+#List the Products Ordered in a Period - LeetCode sql 문제
