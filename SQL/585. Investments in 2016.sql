8f51561b9dbc32ff7889565e179b6f2505d328ed
Abduzhalilova Mukhabbat <148844893+Mukhabbat01@users.noreply.github.com>
2024-11-22 19:17:22 +0900

    Create 585. Investments in 2016.sql

---
+++ SQL/585. Investments in 2016.sql
@@ -0,0 +1,5 @@
+select round(sum(tiv_2016), 2) as tiv_2016
+from insurance
+where tiv_2015 in (select tiv_2015 from insurance 
+                    group by tiv_2015 having count(tiv_2015)>1)
+and concat(lat, lon) not in (select concat(lat, lon) from insurance group by    lat, lon having count(concat(lat, lon)) > 1)
